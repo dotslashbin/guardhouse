5c597f67715d4cd292ae6bc441ce3fbf7bff9760
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-09 12:07:28 +0800

    Added default configs

--- guardhouse_spa/config/app.php
+++ guardhouse_spa/config/app.php
@@ -13,7 +13,10 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Guardhouse'),
+
+    'default_page' => 1,
+    'default_page_limit' => 10,
 
     /*
     |--------------------------------------------------------------------------
